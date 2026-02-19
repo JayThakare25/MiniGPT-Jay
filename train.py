63f955003e814dda499c087ce9018532e1e59fff
jay <jaydarknightx@gmail.com>
2026-02-20 01:04:21 +0530

    Updated max_iters

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ n_embd = 384
 n_heads = 6
 n_layers = 6
 learning_rate = 3e-4
-max_iters = 1000
+max_iters = 4000
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # -------- LOAD DATASET --------
