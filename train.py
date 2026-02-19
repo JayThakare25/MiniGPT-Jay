95109d0fc487cbcaa1b1611cf2b4e18d7faca91c
jay <jaydarknightx@gmail.com>
2026-02-19 23:39:14 +0530

    updating para in trin.py

--- train.py
+++ train.py
@@ -7,12 +7,12 @@ import requests
 
 # -------- CONFIG --------
 block_size = 128
-batch_size = 32
+batch_size = 64
 n_embd = 384
 n_heads = 6
 n_layers = 6
 learning_rate = 3e-4
-max_iters = 200
+max_iters = 1000
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # -------- LOAD DATASET --------
